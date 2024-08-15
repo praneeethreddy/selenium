158252b8c993536eba6fa0b3746fb9b1514c5895
praneethkesarapu <praneeth.kesarapu@gmail.com>
2024-08-15 15:15:41 -0500

    "String and Operators"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,27 @@
 package org.example;
 
-import java.lang.reflect.Array;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        short abc =12;
-        byte ab =122;
-        int a=123234;
+        short abc = 12;
+        byte ab = 122;
+        int a = 123234;
         long b = 1234567876543L;
         float c = 123.23f;
-        double d =1231.23234;
-        String e ="Hello ";
-        int[] a = new int[5];
-        a =new int []{1,2,34,5};
-        Set <integer> myset = new Hashset<>();
+        double d = 1231.23234;
+        String e = "Hello ";
+
+        Set<Integer> myset = new HashSet<>();
         myset.add(12);
         myset.add(22);
         myset.add(32);
+        System.out.println(myset);
+
         System.out.println("Thank You");
         System.out.println("Welcome");
-
-
-
-
-
     }
 }
